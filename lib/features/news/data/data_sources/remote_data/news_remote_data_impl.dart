ef4a721247047b33b219a6b000643d8b00ebf4d8
mohamed <mt917154@gmail.com>
2026-05-03 15:13:23 +0300

    ++

--- lib/features/news/data/data_sources/remote_data/news_remote_data_impl.dart
+++ lib/features/news/data/data_sources/remote_data/news_remote_data_impl.dart
@@ -1,9 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:news_app/core/utils/constants/app_text.dart';
 import 'package:news_app/core/utils/exceptions/dio_exceptions.dart';
-import 'package:news_app/core/utils/exceptions/failure.dart';
 import 'package:news_app/core/utils/services/api_service.dart';
 import 'package:news_app/features/news/data/data_sources/remote_data/news_remote_data.dart';
 import 'package:news_app/features/news/data/models/article/article_model.dart';
@@ -19,8 +17,8 @@ class NewsRemoteDataImpl implements NewsRemoteData {
   Future<List<ArticleModel>> fetchCategoryArticles({
     required String category,
   }) async {
-    final List<ConnectivityResult> connectivityResult =
-        await (Connectivity().checkConnectivity());
+    final List<ConnectivityResult> connectivityResult = await (Connectivity()
+        .checkConnectivity());
 
     if (connectivityResult.contains(ConnectivityResult.none)) {
       throw DioExceptions(errorMessage: AppText.networkError);
@@ -30,20 +28,16 @@ class NewsRemoteDataImpl implements NewsRemoteData {
       queryParams: {"category": category.toLowerCase()},
     );
 
-    return result.fold(
-      (failure) => throw failure,
-      (categoryData) {
-        if (categoryData.data == null || categoryData.data["articles"] == null) {
-          return [];
-        }
-        final listOfArticles = (categoryData.data["articles"] as List)
-            .map(
-              (article) =>
-                  ArticleModel.fromJson(article as Map<String, dynamic>),
-            )
-            .toList();
-        return listOfArticles;
-      },
-    );
+    return result.fold((failure) => throw failure, (categoryData) {
+      if (categoryData.data == null || categoryData.data["articles"] == null) {
+        return [];
+      }
+      final listOfArticles = (categoryData.data["articles"] as List)
+          .map(
+            (article) => ArticleModel.fromJson(article as Map<String, dynamic>),
+          )
+          .toList();
+      return listOfArticles;
+    });
   }
 }
